6a92f9faaad64a7929bc1670c60eaca948dc16cc
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-03 07:20:02 +0500

    update

---
+++ Day_02/puzzle_01.py
@@ -0,0 +1,32 @@
+
+ans = 0
+
+with open("./day_02.in") as fin:
+    lines = fin.read().strip().split("\n")
+
+def is_safe(nums):
+    inc = nums[1] > nums[0]
+
+    if inc:
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i - 1]
+
+            if not 1 <= diff <= 3:
+                return False
+            
+        return True
+    else:
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i - 1]
+
+            if not -3 <= diff <= -1:
+                return False
+            
+        return True
+    
+
+for line in lines:
+    nums =  [int(i) for i in line.split()]
+    ans += is_safe(nums)
+
+print(ans) # ans = 411
---
+++ Day_02/puzzle_02.py
@@ -0,0 +1,40 @@
+ans = 0
+
+with open("./day_02.in") as fin:
+    lines = fin.read().strip().split("\n")
+
+def is_safe(nums):
+    inc = nums[1] > nums[0]
+
+    if inc:
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i - 1]
+
+            if not 1 <= diff <= 3:
+                return False
+            
+        return True
+    else:
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i - 1]
+
+            if not -3 <= diff <= -1:
+                return False
+            
+        return True
+    
+def is_really_safe(nums):
+    if is_safe(nums):
+        return True
+
+    for i in range(len(nums)):
+        if is_safe(nums[:i] + nums[i+1:]):
+            return True
+        
+    return False
+
+for line in lines:
+    nums =  [int(i) for i in line.split()]
+    ans += is_really_safe(nums)
+
+print(ans) # ans = 465
