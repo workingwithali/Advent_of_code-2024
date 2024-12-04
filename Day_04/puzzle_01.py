3b29087b19ce15976817dffb3aa7e6a3ef30bea4
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-04 16:02:05 +0500

    update

--- Day_04/puzzle_01.py
+++ Day_04/puzzle_01.py
@@ -1,3 +1,32 @@
 
 with open(r"d:\Advent_of_code-2024\Day_04\day_04.in") as fin:
-    line = fin.read().strip()
+    lines = fin.read().strip().split('\n')
+
+n = len(lines)
+m = len(lines[0])
+
+# Generate all directions
+dd = []
+for dx in range(-1, 2):
+    for dy in range(-1, 2):
+        if dx != 0 or dy != 0:
+            dd.append((dx, dy))
+
+def has_xmas(i, j, d):
+    dx, dy = d
+    for k, x in enumerate("XMAS"):
+        ii = i + k * dx
+        jj = j + k * dy
+        if not (0 <= ii < n and 0 <= jj < m):
+            return False
+        if lines[ii][jj] != x:
+            return False
+    return True
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        for d in dd:
+            ans += has_xmas(i, j, d)
+
+print(ans)
---
+++ Day_04/puzzle_02.py
@@ -0,0 +1,30 @@
+with open(r"d:\Advent_of_code-2024\Day_04\day_04.in") as fin:
+    lines = fin.read().strip().split("\n")
+
+n = len(lines)
+m = len(lines[0])
+
+dd = []
+for dx in range(-1, 2):
+    for dy in range(-1, 2):
+        if dx != 0 or dy != 0:
+            dd.append((dx, dy))
+
+def has_xmas(i, j):
+    if not (1 <= i < n - 1 and 1 <= j < m - 1):
+        return False
+    if lines[i][j] != "A":
+        return False
+
+    # Check both diagonals
+    diag_1 = f"{lines[i-1][j-1]}{lines[i+1][j+1]}"
+    diag_2 = f"{lines[i-1][j+1]}{lines[i+1][j-1]}"
+
+    return diag_1 in ["MS", "SM"] and diag_2 in ["MS", "SM"]
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        ans += has_xmas(i, j)
+
+print(ans)
