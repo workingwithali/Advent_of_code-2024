4dbea08cb29637845c03e122c68335554b1f90d3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-04 07:01:43 +0500

    update

---
+++ Day_03/puzzle_02.py
@@ -0,0 +1,20 @@
+import re
+
+with open("./day_03.in") as fin:
+    line = fin.read().strip()
+
+matches = re.findall(r"(?:mul\((\d+),(\d+)\))|(do\(\)|don't\(\))", line)
+
+enabled = True
+
+ans = 0
+for match in matches:
+    if match[2] == "" and enabled:
+        ans += int(match[0]) * int(match[1])
+    else:
+        if match[2] == "do()":
+            enabled = True
+        else:
+            enabled = False
+
+print(ans) # ans = 82868252
