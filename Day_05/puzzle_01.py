76a8fa7b93a41ef8791d0e1564a4c7ce786440e5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-05 15:44:05 +0500

    update

--- Day_05/puzzle_01.py
+++ Day_05/puzzle_01.py
@@ -1,24 +1,17 @@
 with open(r"d:\Advent_of_code-2024\Day_05\day_05.in") as fin:
-    content = fin.read().strip()
+    raw_rules, updates = fin.read().strip().split("\n\n")
+    rules = []
+    for line in raw_rules.split("\n"):
+        a, b = line.split("|")
+        rules.append((int(a), int(b)))
+    updates = [list(map(int, line.split(","))) for line in updates.split("\n")]
 
-# Split the content into rules and updates sections
-if "\n\n" in content:
-    raw_rules, updates = content.split("\n\n")
-else:
-    raise ValueError("Input file does not contain two sections separated by a blank line.")
 
-# Parse rules
-rules = []
-for line in raw_rules.split("\n"):
-    a, b = line.split("|")
-    rules.append((int(a), int(b)))
-
-# Parse updates
-updates = [list(map(int, line.split(","))) for line in updates.split("\n")]
 
 def follows_rules(update):
     idx = {}
     for i, num in enumerate(update):
+        print(num)
         idx[num] = i
     
     for a, b in rules:
