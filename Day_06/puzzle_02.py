3c7b9f1e84fcbd8be3fb547605069bd1780463a8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-07 07:06:51 +0500

    update

---
+++ Day_06/puzzle_02.py
@@ -0,0 +1,10 @@
+with open(r"d:\Advent_of_code-2024\Day_06\day_06.in") as fin:
+    grid = fin.read().strip().split("\n")
+    
+
+
+n = len(grid)
+m = len(grid[0])
+print(n)
+print(m)
+found = False
