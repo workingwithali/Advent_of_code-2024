d984ed158b51bae4a8c470b245c4dbbb02ffd232
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-07 07:20:48 +0500

    update

--- Day_06/puzzle_02.py
+++ Day_06/puzzle_02.py
@@ -5,6 +5,19 @@ with open(r"d:\Advent_of_code-2024\Day_06\day_06.in") as fin:
 
 n = len(grid)
 m = len(grid[0])
-print(n)
-print(m)
+
 found = False
+for i in range(n):
+    for j in range(m):
+        if grid[i][j] == "^":
+            found = True
+            print(i)
+            print(j)
+            break
+    if found:
+        break
+
+
+
+
+    
